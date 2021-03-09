c84a8f00257c1bdbc8e2d8074b20dbc7aab5946f
theluanz <luanzuffo.lz@gmail.com>
2021-03-09 19:50:52 -0300

    migrates atualizadas

--- database/migrations/2021_03_09_195023_create_shopping_carts_table.php
+++ database/migrations/2021_03_09_195023_create_shopping_carts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateShoppingCartsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('shopping_carts', function (Blueprint $table) {
             $table->id();
-            $table->string('description');
-            $table->float('price');
-            $table->text('details')->nullable();
-            $table->foreignId('user_id')->constrained();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -31,6 +26,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('shopping_carts');
     }
 }
